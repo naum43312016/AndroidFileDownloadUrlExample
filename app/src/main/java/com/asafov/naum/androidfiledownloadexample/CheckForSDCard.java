44f02a581c4648cf96011d67d19f4ad41e64a29e
Naum Asafov <naum43312016@gmail.com>
2018-11-30 22:28:20 +0200

    Initial commit

---
+++ app/src/main/java/com/asafov/naum/androidfiledownloadexample/CheckForSDCard.java
@@ -0,0 +1,14 @@
+package com.asafov.naum.androidfiledownloadexample;
+
+import android.os.Environment;
+
+public class CheckForSDCard {
+    public static boolean isSDCardPresent() {
+        if (Environment.getExternalStorageState().equals(
+
+                Environment.MEDIA_MOUNTED)) {
+            return true;
+        }
+        return false;
+    }
+}
